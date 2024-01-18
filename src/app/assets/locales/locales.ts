adebd82e2c8a5a8f23c1af1a4d9d0e508c80217b
Rasheduluctit <rasheduli.uctitbd@gmail.com>
2024-01-18 15:34:26 +0600

    initial setup done

---
+++ src/app/assets/locales/locales.ts
@@ -0,0 +1,15 @@
+import LocalizedStrings from "react-localization";
+
+export let strings = new LocalizedStrings({
+  en: {
+    home: {},
+    company: {
+      about: {},
+      values: {},
+      whyChooseUs: {},
+      pricing: {},
+    },
+    services: {},
+    caseStudies: {},
+  },
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#8000ff",
+        secondary: "#180030",
+        textColor: "#7987a1",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(40px)" },
+          "50%": { opacity: "0.5" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.5s ease-in-out",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
