ee4205c4ab92a69fa521066be224582bb96a4ba4
Rasheduluctit <rasheduli.uctitbd@gmail.com>
2024-01-20 16:49:47 +0600

    initial design

--- src/app/assets/locales/locales.ts
+++ src/app/assets/locales/locales.ts
@@ -2,6 +2,46 @@ import LocalizedStrings from "react-localization";
 
 export let strings = new LocalizedStrings({
   en: {
+    navbar: {
+      logoAltText: "UCTIT BD Official Logo",
+      buttonText: "Get in Touch",
+      buttonIconAltText: "Right Arrow Sign",
+      navs: {
+        home: {
+          navTitle: "Home",
+          url: "/",
+        },
+        company: {
+          navTitle: "Company",
+          subNavs: [
+            {
+              navTitle: "About Us",
+              url: "#",
+            },
+            {
+              navTitle: "Our Values",
+              url: "#",
+            },
+            {
+              navTitle: "Why Choose Us",
+              url: "#",
+            },
+            {
+              navTitle: "Pricing",
+              url: "#",
+            },
+          ],
+        },
+        services: {
+          navTitle: "Services",
+          url: "#",
+        },
+        caseStudies: {
+          navTitle: "Case Studies",
+          url: "#",
+        },
+      },
+    },
     home: {},
     company: {
       about: {},
