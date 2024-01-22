f6c7f97d3a6a4982b797f3c96ee59d8606ecc643
Rasheduluctit <rasheduli.uctitbd@gmail.com>
2024-01-22 17:27:50 +0600

    nav completed

--- src/app/components/navbar/DropdownMenu.tsx
+++ src/app/components/navbar/DropdownMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { strings } from "@/app/assets/locales/locales";
 import React, { useState } from "react";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 const DropdownMenu = () => {
   const localeText = strings.navbar.navs.company;
@@ -8,22 +9,27 @@ const DropdownMenu = () => {
   return (
     <React.Fragment>
       <li
-        className="relative h-full flex items-center"
+        className="relative h-full flex lg:items-center flex-col lg:flex-row text-secondary"
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
       >
-        <p className="hover:text-primary ">{localeText.navTitle}</p>
+        <p className="hover:text-primary group flex items-center justify-between lg:block">
+          <span>{localeText.navTitle}</span>
+          <span className="lg:hidden group-hover:-rotate-180 duration-300 ease-in-out">
+            <MdOutlineKeyboardArrowDown />
+          </span>
+        </p>
         {hovered && (
           <ul
-            className={` bg-white rounded-lg border-[1px] border-gray-300 absolute top-20 px-5 py-4 w-[250px] space-y-5`}
+            className={` bg-white rounded-lg lg:border-[1px] lg:border-gray-300 lg:absolute top-20 px-5 py-4 w-full lg:w-[220px] space-y-3 lg:space-y-5 duration-300`}
           >
             {localeText.subNavs.map((subnav) => (
               <li
                 key={subnav.navTitle}
                 className="group flex items-center gap-1"
               >
-                <span className="h-[2px] w-0 group-hover:w-[18px] bg-primary transition-all duration-300"></span>
-                <p className="group-hover:ml-2 transition-all">
+                <span className="hidden lg:inline h-[2px] w-0 group-hover:w-[18px] bg-primary transition-all duration-300"></span>
+                <p className="lg:group-hover:ml-2 group-hover:text-primary transition-all">
                   {subnav.navTitle}
                 </p>
               </li>
--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,23 +1,24 @@
-"use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 import logo from "../../../../public/images/icons/logo.webp";
 import { IoMdArrowForward } from "react-icons/io";
+
 import { strings } from "@/app/assets/locales/locales";
 import DropdownMenu from "./DropdownMenu";
+import MobileNavbar from "./MobileNavbar";
 
 const Navbar = () => {
   const localeText = strings.navbar;
 
   return (
     <React.Fragment>
-      <nav className="flex items-center justify-between font-medium text-lg border-b-[1px] border-gray-300 pl-[50px] h-[91px]">
+      <nav className="flex items-center justify-between font-medium text-lg border-b-[1px] border-gray-300 px-5 lg:px-0 lg:pl-[50px] h-[91px]">
         <Image
           className="w-16 h-16"
           src={logo}
           alt={localeText.logoAltText}
         ></Image>
-        <ul className="flex items-center gap-5 text-secondary transition-all duration-500 delay-200 ease-in-out cursor-pointer h-full">
+        <ul className="hidden lg:flex items-center gap-5 text-secondary transition-all duration-500 delay-200 ease-in-out cursor-pointer h-full">
           <li className="hover:text-primary">
             {localeText.navs.home.navTitle}
           </li>
@@ -29,10 +30,11 @@ const Navbar = () => {
             {localeText.navs.caseStudies.navTitle}
           </li>
         </ul>
-        <button className="group bg-primary text-white h-full flex items-center gap-12 px-7 py-4 hover:bg-white hover:text-secondary  duration-500 ease-in-out">
+        <button className="group bg-primary text-white h-full hidden lg:flex items-center gap-12 px-7 py-4 hover:bg-white hover:text-secondary  duration-500 ease-in-out">
           <span>{localeText.buttonText}</span>
           <IoMdArrowForward className="w-5 h-5 rotate-45 group-hover:rotate-0 transition-all" />
         </button>
+        <MobileNavbar />
       </nav>
     </React.Fragment>
   );
